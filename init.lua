e598bf7cbdb50a857ac24cf848cef14ae953002a
algol <algol.mail@gmail.com>
2023-03-18 14:02:24 +0700

    update configs.

--- init.lua
+++ init.lua
@@ -41,46 +41,43 @@ return {
       },
     },
   },
-
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
   -- Set colorscheme to use
+  -- colorscheme = "catppuccin",
   colorscheme = "sonokai",
   -- colorscheme = "astrodark",
-
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = {
       source = "always", -- Or "if_many"
-      prefix = "●", -- Could be '■', '▎', 'x'
+      prefix = "●",    -- Could be '■', '▎', 'x'
     },
     severity_sort = true,
     underline = true,
     signs = { active = true },
   },
-
   lsp = {
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -102,7 +99,6 @@ return {
       "gopls",
     },
   },
-
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -113,7 +109,6 @@ return {
       },
     },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
--- options.lua
+++ options.lua
@@ -1,34 +1,34 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
-  opt = {
-    -- set to true or false etc.
-    relativenumber = false, -- sets vim.opt.relativenumber
-    number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
-    showtabline = 4,
-    tabstop = 4,
-    softtabstop = 4,
-    shiftwidth = 4,
-    colorcolumn = "120",
-    clipboard = "unnamedplus",
-    list = true,
-    listchars = "tab:» ,space:·,extends:<,precedes:>,conceal:┊,nbsp:␣",
-  },
-  g = {
-    mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true, -- enable completion at start
-    autopairs_enabled = true, -- enable autopairs at start
-    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-    ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-
-    sonokai_style = "atlantis",
-    sonokai_better_performance = 1,
-    sonokai_enable_italic = 1,
-  },
+    opt = {
+        -- set to true or false etc.
+        relativenumber = false, -- sets vim.opt.relativenumber
+        number = true,      -- sets vim.opt.number
+        spell = false,      -- sets vim.opt.spell
+        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        wrap = false,       -- sets vim.opt.wrap
+        termguicolors = true,
+        showtabline = 4,
+        tabstop = 4,
+        softtabstop = 4,
+        shiftwidth = 4,
+        colorcolumn = "120",
+        clipboard = "unnamedplus",
+        list = true,
+        listchars = "tab:» ,space:·,extends:<,precedes:>,conceal:┊,nbsp:␣",
+    },
+    g = {
+        mapleader = " ",             -- sets vim.g.mapleader
+        autoformat_enabled = true,   -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+        cmp_enabled = true,          -- enable completion at start
+        autopairs_enabled = true,    -- enable autopairs at start
+        diagnostics_mode = 3,        -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+        icons_enabled = true,        -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+        ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+        sonokai_style = "atlantis",
+        sonokai_better_performance = 1,
+        sonokai_enable_italic = 1,
+    },
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
@@ -37,5 +37,5 @@ return {
 --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
 --   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
 --
---   return local_vim
+-- return local_vim
 -- end
