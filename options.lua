68dd185f1cc85189a59fe3237f154ef16aae75eb
algol <algol.mail@gmail.com>
2023-08-09 09:31:11 +0700

    add spell checker.

--- options.lua
+++ options.lua
@@ -3,10 +3,11 @@ return {
   opt = {
     -- set to true or false etc.
     relativenumber = false, -- sets vim.opt.relativenumber
-    number = true,          -- sets vim.opt.number
-    spell = false,          -- sets vim.opt.spell
-    signcolumn = "auto",    -- sets vim.opt.signcolumn to auto
-    wrap = false,           -- sets vim.opt.wrap
+    number = true, -- sets vim.opt.number
+    spell = true, -- sets vim.opt.spell
+    spelllang = { "en", "ru" },
+    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    wrap = false, -- sets vim.opt.wrap
     termguicolors = true,
     showtabline = 4,
     tabstop = 4,
@@ -16,18 +17,17 @@ return {
     clipboard = "unnamedplus",
     list = true,
     listchars = "eol:↲,tab:» ,trail:·,space:·,extends:<,precedes:>,conceal:┊,nbsp:␣",
-    langmap =
-    "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz",
+    langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯЖ;ABCDEFGHIJKLMNOPQRSTUVWXYZ:,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz",
   },
   g = {
-    mapleader = " ",                 -- sets vim.g.mapleader
-    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true,              -- enable completion at start
-    autopairs_enabled = true,        -- enable autopairs at start
-    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ", -- sets vim.g.mapleader
+    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true, -- enable completion at start
+    autopairs_enabled = true, -- enable autopairs at start
+    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    sonokai_style = "andromeda",     -- andromeda | atlantis | espresso | maia | shusia
+    sonokai_style = "andromeda", -- andromeda | atlantis | espresso | maia | shusia
     -- sonokai_better_performance = 1,
     sonokai_enable_italic = 1,
   },
