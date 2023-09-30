407c94024c8ada5e2fa2056c0375706cb37fbde7
algol <algol.mail@gmail.com>
2023-09-30 21:39:12 +0700

    update config.

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,16 +6,16 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.colorscheme.everforest" },
-  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
   { import = "astrocommunity.syntax.hlargs-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.lsp.inc-rename-nvim" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.debugging.nvim-bqf" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
 
   {
     "m-demare/hlargs.nvim",
