126006a216f741dd67174f50e34f686d7a0989e2
algol <algol1981@yandex.ru>
2024-10-17 22:37:04 +0700

    update pls config

--- lua/plugins/pls.lua
+++ lua/plugins/pls.lua
@@ -1,16 +1,16 @@
 return {
   -- "AstroNvim/astrolsp",
   -- -- https://github.com/lasorda/protobuf-language-server
+
   -- ---@type AstroLSPOpts
   -- opts = function(plugin, opts)
-  --   --   --   -- insert "prolog_lsp" into our list of servers
   --   opts.servers = opts.servers or {}
-  --   table.insert(opts.servers, "pls")
+  --   table.insert(opts.servers, "protobuf_language_server")
   --
   --   opts.config = require("astrocore").extend_tbl(opts.config or {}, {
   --     --   -- this must be a function to get access to the `lspconfig` module
-  --     pls = {
-  --       cmd = { "pls" },
+  --     protobuf_language_server = {
+  --       cmd = { "protobuf-language-server" },
   --       filetypes = { "proto", "cpp" },
   --       root_dir = require("lspconfig.util").root_pattern ".git",
   --       single_file_support = true,
@@ -18,6 +18,7 @@ return {
   --         ["additional-proto-dirs"] = {
   --           -- path to additional protobuf directories
   --           "vendor",
+  --           "vendor.protogen",
   --           -- "third_party",
   --         },
   --       },
