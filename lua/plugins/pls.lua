415963282366c362d98d38434db808984a4bba0c
algol <algol1981@yandex.ru>
2024-10-19 20:05:06 +0700

    fix proto lsp config

--- lua/plugins/pls.lua
+++ lua/plugins/pls.lua
@@ -1,46 +1,28 @@
-require("lspconfig.configs").protobuf_language_server = {
-  default_config = {
-    cmd = { "protobuf-language-server" },
-    filetypes = { "proto", "cpp" },
-    root_fir = require("lspconfig.util").root_pattern ".git",
-    single_file_support = true,
-    settings = {
-      ["additional-proto-dirs"] = {
-        -- path to additional protobuf directories
-        "vendor",
-        "vendor.protogen",
-        -- "third_party",
-      },
-    },
-  },
-}
-require("lspconfig").protobuf_language_server.setup {}
-
 return {
-  -- "AstroNvim/astrolsp",
-  -- -- https://github.com/lasorda/protobuf-language-server
+  "AstroNvim/astrolsp",
+  -- https://github.com/lasorda/protobuf-language-server
 
-  -- ---@type AstroLSPOpts
-  -- opts = function(plugin, opts)
-  --   opts.servers = opts.servers or {}
-  --   table.insert(opts.servers, "protobuf_language_server")
-  --
-  --   opts.config = require("astrocore").extend_tbl(opts.config or {}, {
-  --     --   -- this must be a function to get access to the `lspconfig` module
-  --     protobuf_language_server = {
-  --       cmd = { "protobuf-language-server" },
-  --       filetypes = { "proto", "cpp" },
-  --       root_dir = require("lspconfig.util").root_pattern ".git",
-  --       single_file_support = true,
-  --       settings = {
-  --         ["additional-proto-dirs"] = {
-  --           -- path to additional protobuf directories
-  --           "vendor",
-  --           "vendor.protogen",
-  --           -- "third_party",
-  --         },
-  --       },
-  --     },
-  --   })
-  -- end,
+  ---@type AstroLSPOpts
+  opts = function(plugin, opts)
+    opts.servers = opts.servers or {}
+    table.insert(opts.servers, "protobuf_language_server")
+
+    opts.config = require("astrocore").extend_tbl(opts.config or {}, {
+      --   -- this must be a function to get access to the `lspconfig` module
+      protobuf_language_server = {
+        cmd = { "protobuf-language-server" },
+        filetypes = { "proto", "cpp" },
+        root_dir = require("lspconfig.util").root_pattern ".git",
+        single_file_support = true,
+        settings = {
+          ["additional-proto-dirs"] = {
+            -- path to additional protobuf directories
+            "vendor",
+            "vendor.protogen",
+            -- "third_party",
+          },
+        },
+      },
+    })
+  end,
 }
