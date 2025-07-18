d5d07e11ca735e9214ce9ea95ff78e0cb236e7ed
algol <algol1981@yandex.ru>
2025-07-18 12:58:04 +0700

    Fix clipboard

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -46,6 +46,7 @@ return {
         -- configure global vim variables (vim.g)
         -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
         -- This can be found in the `lua/lazy_setup.lua` file
+        clipboard = "wl-copy",
       },
       o = {
         background = "light",
