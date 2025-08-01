2eec241fcabf78f36f57bea765c1a3d8f70d2c3b
algol <algol1981@yandex.ru>
2025-08-01 23:26:36 +0700

    Update codecompanion config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -5,18 +5,19 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "ravitemer/codecompanion-history.nvim",
-      -- "ravitemer/mcphub.nvim",
     },
     opts = {
       adapters = {
+        show_defaults = true,
+        show_model_choices = true,
         bothub = function()
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
-              url = "https://bothub.chat/api/v2/openai", -- optional: default value is ollama url http://127.0.0.1:11434
+              url = "https://bothub.chat/api/v2", -- optional: default value is ollama url http://127.0.0.1:11434
               api_key = "cmd: gpg --batch --quiet --decrypt ~/.config/nvim/bothub_api_key.gpg",
               -- chat_url = "/v1/chat/completions", -- optional: default value, override if different
-              -- models_endpoint = "/v1/models", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
-              models_endpoint = "https://bothub.chat/api/v2/model/list?children=1", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
+              -- models_endpoint = "/model/list?children=1", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
+              -- models_endpoint = "https://bothub.chat/api/v2/model/list?children=1", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
             },
             schema = {
               model = {
@@ -190,37 +191,6 @@ When given a task:
 2. Output the code in a single code block, being careful to only return relevant code.
 3. You should always generate short suggestions for the next user turns that are relevant to the conversation.
 4. You can only give one reply for each conversation turn.]]
-
-        --       return [[You are an AI programming assistant named "CodeCompanion". You are currently plugged in to the Neovim text editor on a user's machine.
-        --
-        -- Your core tasks include:
-        -- - Answering general programming questions.
-        -- - Explaining how the code in a Neovim buffer works.
-        -- - Reviewing the selected code in a Neovim buffer.
-        -- - Generating unit tests for the selected code.
-        -- - Proposing fixes for problems in the selected code.
-        -- - Scaffolding code for a new workspace.
-        -- - Finding relevant code to the user's query.
-        -- - Proposing fixes for test failures.
-        --
-        -- You must:
-        -- - Follow the user's requirements carefully and to the letter.
-        -- - Keep your answers short and impersonal, especially if the user responds with context outside of your tasks.
-        -- - Minimize other prose.
-        -- - Use Markdown formatting in your answers.
-        -- - Include the programming language name at the start of the Markdown code blocks.
-        -- - Avoid including line numbers in code blocks.
-        -- - Avoid wrapping the whole response in triple backticks.
-        -- - Only return code that's relevant to the task at hand. You may not need to return all of the code that the user has shared.
-        -- - Use actual line breaks instead of '\n' in your response to begin new lines.
-        -- - Use '\n' only when you want a literal backslash followed by a character 'n'.
-        -- - All non-code responses must be in %s.
-        --
-        -- When given a task:
-        -- 1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail, unless asked not to do so.
-        -- 2. Output the code in a single code block, being careful to only return relevant code.
-        -- 3. You should always generate short suggestions for the next user turns that are relevant to the conversation.
-        -- 4. You can only give one reply for each conversation turn.]]
       end,
     },
   },
