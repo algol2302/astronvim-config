bf2f281da357a14f18fbc669a3d8f0182d4068bc
algol <algol1981@yandex.ru>
2025-08-03 16:40:14 +0700

    feat: added yandex_gpt adapter and updated configurations

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,3 +1,5 @@
+local secret = require "helpers.secret"
+
 return {
   {
     "codecompanion.nvim",
@@ -9,9 +11,8 @@ return {
     opts = {
       adapters = {
         show_defaults = true,
-        show_model_choices = true,
+        show_model_choices = false,
         bothub = function()
-          local secret = require "helpers.secret"
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
               url = "https://bothub.chat/api/v2", -- optional: default value is ollama url http://127.0.0.1:11434
@@ -71,6 +72,64 @@ return {
             },
           })
         end,
+        yandex_gpt = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://llm.api.cloud.yandex.net",
+              api_key = secret.load "~/.config/nvim/ya_api_key.gpg",
+              -- chat_url = "/openai/v1/chat/completions", -- optional: default value, override if different
+              -- models_endpoint = "/model/list?children=1", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
+              -- models_endpoint = "https://bothub.chat/api/v2/model/list?children=1", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
+            },
+            schema = {
+              model = {
+                default = "gpt://" .. secret.load "~/.config/nvim/ya_dir.gpg" .. "/yandexgpt",
+              },
+              temperature = {
+                order = 2,
+                mapping = "parameters",
+                type = "number",
+                optional = true,
+                default = 0.8,
+                desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
+                validate = function(n) return n >= 0 and n <= 2, "Must be between 0 and 2" end,
+              },
+              max_completion_tokens = {
+                order = 3,
+                mapping = "parameters",
+                type = "integer",
+                optional = true,
+                default = nil,
+                desc = "An upper bound for the number of tokens that can be generated for a completion.",
+                validate = function(n) return n > 0, "Must be greater than 0" end,
+              },
+              stop = {
+                order = 4,
+                mapping = "parameters",
+                type = "string",
+                optional = true,
+                default = nil,
+                desc = "Sets the stop sequences to use. When this pattern is encountered the LLM will stop generating text and return. Multiple stop patterns may be set by specifying multiple separate stop parameters in a modelfile.",
+                validate = function(s) return s:len() > 0, "Cannot be an empty string" end,
+              },
+              logit_bias = {
+                order = 5,
+                mapping = "parameters",
+                type = "map",
+                optional = true,
+                default = nil,
+                desc = "Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID) to an associated bias value from -100 to 100. Use https://platform.openai.com/tokenizer to find token IDs.",
+                subtype_key = {
+                  type = "integer",
+                },
+                subtype = {
+                  type = "integer",
+                  validate = function(n) return n >= -100 and n <= 100, "Must be between -100 and 100" end,
+                },
+              },
+            },
+          })
+        end,
       },
       -- mcphub = {
       --   callback = "mcphub.extensions.codecompanion",
@@ -82,13 +141,13 @@ return {
       -- },
       strategies = {
         chat = {
-          adapter = "bothub",
+          adapter = "yandex_gpt",
         },
         inline = {
-          adapter = "bothub",
+          adapter = "yandex_gpt",
         },
         cmd = {
-          adapter = "bothub",
+          adapter = "yandex_gpt",
         },
       },
       display = {
