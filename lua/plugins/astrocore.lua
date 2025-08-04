eec86bd28fdcd61d66d0f4d897e1aa57f4a77e38
algol <algol1981@yandex.ru>
2025-08-04 17:36:14 +0700

    Fix misclick behavior

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -115,6 +115,8 @@ return {
         },
         ["<leader>f;"] = { "<cmd>vimgrep /t.Run(/g %<cr><cr>", desc = "Show all go subtests in quickfix list" },
 
+        ["<M-s>"] = { ":w!<cr>", desc = "Save File" },
+
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
