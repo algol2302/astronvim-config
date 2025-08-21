28144bb0e6be51d9c15ad87e171430365620bdde
algol <algol1981@yandex.ru>
2025-08-21 22:02:03 +0700

    Decrease temperature for codecompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -78,7 +78,7 @@ local temperature = {
   mapping = "parameters",
   type = "number",
   optional = true,
-  default = 0.8,
+  default = 0.2,
   desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
   validate = function(n) return n >= 0 and n <= 2, "Must be between 0 and 2" end,
 }
