00f3e7474e9c5f22ea6075798e9ab2742d6c9915
algol <algol1981@yandex.ru>
2025-09-07 15:00:48 +0700

    Update dashboard

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -27,6 +27,44 @@ return {
     opts = {
       dashboard = {
         preset = {
+          keys = {
+            {
+              key = "n",
+              action = "<Leader>n",
+              icon = require("astroui").get_icon("FileNew", 0, true),
+              desc = "New File  ",
+            },
+            {
+              key = "f",
+              action = "<Leader>ff",
+              icon = require("astroui").get_icon("Search", 0, true),
+              desc = "Find File  ",
+            },
+            {
+              key = "o",
+              action = "<Leader>fo",
+              icon = require("astroui").get_icon("DefaultFile", 0, true),
+              desc = "Recents  ",
+            },
+            {
+              key = "w",
+              action = "<Leader>fw",
+              icon = require("astroui").get_icon("WordFile", 0, true),
+              desc = "Find Word  ",
+            },
+            {
+              key = "'",
+              action = "<Leader>f'",
+              icon = require("astroui").get_icon("Bookmarks", 0, true),
+              desc = "Bookmarks  ",
+            },
+            {
+              key = "s",
+              action = "<Leader>S.",
+              icon = require("astroui").get_icon("Refresh", 0, true),
+              desc = "Last Dir Session  ",
+            },
+          },
           header = table.concat({
             [[                                                                       ]],
             [[                                                                     ]],
