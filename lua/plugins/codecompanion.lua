c3917884d0ce0c8db1b5a17a0f5c7c4f1d28d117
algol <algol1981@yandex.ru>
2025-12-14 23:26:49 +0700

    Fix config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -300,7 +300,7 @@ return {
           end,
         },
       },
-      strategies = {
+      interactions = {
         chat = {
           adapter = "z_ai",
         },
@@ -406,50 +406,10 @@ return {
             make_slash_commands = true, -- Add MCP prompts as /slash commands
           },
         },
-        -- vectorcode = {
-        --   ---@type VectorCode.CodeCompanion.ExtensionOpts
-        --   opts = {
-        --     tool_group = {
-        --       -- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
-        --       enabled = true,
-        --       -- a list of extra tools that you want to include in `@vectorcode_toolbox`.
-        --       -- if you use @vectorcode_vectorise, it'll be very handy to include
-        --       -- `file_search` here.
-        --       extras = {},
-        --       collapse = false, -- whether the individual tools should be shown in the chat
-        --     },
-        --     tool_opts = {
-        --       ---@type VectorCode.CodeCompanion.ToolOpts
-        --       ["*"] = {},
-        --       ---@type VectorCode.CodeCompanion.LsToolOpts
-        --       ls = {},
-        --       ---@type VectorCode.CodeCompanion.VectoriseToolOpts
-        --       vectorise = {},
-        --       ---@type VectorCode.CodeCompanion.QueryToolOpts
-        --       query = {
-        --         max_num = { chunk = -1, document = -1 },
-        --         default_num = { chunk = 50, document = 10 },
-        --         include_stderr = false,
-        --         use_lsp = false,
-        --         no_duplicate = true,
-        --         chunk_mode = false,
-        --         ---@type VectorCode.CodeCompanion.SummariseOpts
-        --         summarise = {
-        --           ---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
-        --           enabled = false,
-        --           adapter = nil,
-        --           query_augmented = true,
-        --         },
-        --       },
-        --       files_ls = {},
-        --       files_rm = {},
-        --     },
-        --   },
-        -- },
       },
       prompt_library = {
         ["Code Edit"] = {
-          strategy = "inline",
+          interaction = "inline",
           description = "Prompt the LLM from Neovim",
           opts = {
             is_slash_cmd = false,
@@ -473,7 +433,7 @@ return {
           },
         },
         ["Text Fix Spelling Inline"] = {
-          strategy = "inline",
+          interaction = "inline",
           opts = {
             is_slash_cmd = false,
             user_prompt = false,
